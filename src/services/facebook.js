0c03ad7ad53d3834a6f37406a8cd97f8ef495cd6
Adam Bystran <bystran.adam.21@gmail.com>
2020-09-09 19:39:54 +0200

    hotfix

--- src/services/facebook.js
+++ src/services/facebook.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = "https://victory.srworld.sk/"
+const url = "https://victory.srworld.sk/facebook-posts"
 
 const fetch_fb_data = async () => {
     const res = await axios.get(url);
