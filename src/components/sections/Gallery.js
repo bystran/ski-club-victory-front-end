8b988fdc94762cccd3c80cce1b6dcf90f3f806fd
Adam Bystran <bystran.adam.21@gmail.com>
2020-10-24 12:13:53 +0100

    Adds a way to filter photos and videos in gallery
    Adds a filter bar to Gallery.
        -allows to display both photos and videos
         or each separately
        -fixes height resize issue when displaying videos in
         the gallery component

--- src/components/sections/Gallery.js
+++ src/components/sections/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import useFacebookData from '../../hooks/useFacebookData';
 import ImageGallery from 'react-image-gallery';
 import ReactPlayer from 'react-player'
@@ -10,46 +10,87 @@ import Decoration from '../../assets/icons/decoration.svg';
 import BlueStripe from '../../assets/icons/rectangle_blue.svg';
 import BlueMountainBg from '../BlueMountainBg';
 
+
+
 const Gallery = () => {
     const fbData = useFacebookData();
-    const getImgs = () => {
+    const [filter, setFilter] = useState("all"); // one of ["all", "photos", "videos"]
+    const getImgs = (filterWord) => {
         if(fbData && fbData.media){
-            return fbData.media.map( media => {
+            return fbData.media.reduce( (accumulator, media) => {
                 if(media.media.source){
-                    return {
-                        renderItem: () => <ReactPlayer
-                            stopOnUnmount url={media.media.source}
-                            light={media.media.image.src} 
-                            playing muted
-                            controls
-                        />,
-                        thumbnail:media.media.image.src,   
-                        original:media.media.image.src,
+                    if(filterWord !== "photos"){
+                        return accumulator.concat({
+                            renderItem: () => <ReactPlayer
+                                stopOnUnmount url={media.media.source}
+                                light={media.media.image.src} 
+                                playing
+                                controls
+
+                            />,
+                            thumbnail:media.media.image.src,   
+                            original:media.media.image.src,
+                            originalClass: "react-video-slide"
+                        })
+                    }else{
+                        return accumulator;
                     }
+                }else if(filterWord!== "videos"){
+                    return accumulator.concat({
+                        original:media.media.image.src,
+                        thumbnail:media.media.image.src,     
+                    });
+                }else {
+                    return accumulator;
                 }
-                return {
-                    original:media.media.image.src,
-                    thumbnail:media.media.image.src,     
-                }
-            })
+
+            }, [])
             
         }
         return [];
 
 
     }
-    const imgs = getImgs()
+    const imgs = getImgs(filter);
     return (
+        <>
         <BlueMountainBg className='gallery-section' id='galeria'>
             {/* <BlueStripe className='smaller-svg'/>
             <Decoration className='larger-svg'/> */}
             <h1>Galéria</h1>
+            <div className="gallery-header-wrp">
+                <ul className="gallery-header">
+                    <li 
+                        className = {`${filter === "all" ? "active": ""}`}
+                        onClick = {()=>setFilter("all")}
+                    >
+                        Všetky
+                    </li>
+                    <li 
+                        className={`${filter === "photos" ? "active": ""}`}
+                        onClick = {()=>setFilter("photos")}
+                    >
+                        Fotky
+                    </li>
+                    <li 
+                        className={`${filter === "videos" ? "active": ""}`}
+                        onClick = {()=>setFilter("videos")}
+                    >
+                        Videá
+                    </li>
+                </ul>
+            </div>
+           
             <div className="gallery-content">                  
                 <ImageGallery lazyLoad={true} items={imgs} showPlayButton={false} />
             </div>
 
             
         </BlueMountainBg>
+        <div className="past-images-of-the-week">
+
+        </div>
+        </>
     )
 }
 
